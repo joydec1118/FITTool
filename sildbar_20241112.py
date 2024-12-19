e4247fddd0ed4a55383eb03d19605030553f7087
joydec1118 <joydec1118@hotmail.com>
2024-12-19 13:43:57 +0800

    Update sildbar_20241112.py

--- sildbar_20241112.py
+++ sildbar_20241112.py
@@ -9,18 +9,14 @@ import seaborn as sns
 
 st.write("66666")
 # Add custom CSS to hide the GitHub icon
-st.markdown(
-    """
+st.markdown("""
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+        footer {visibility: hidden;}
+        [data-testid="stToolbarActions"] > div:first-child {
+            visibility: hidden;
+        }
     </style>
-    """,
-    unsafe_allow_html=True
-)
+""", unsafe_allow_html=True)
 
 def data_analysis():
         # Sidebar for primary task selection
