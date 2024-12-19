c81ab762456918071b8d8295bb1ce90cefec6f71
joydec1118 <joydec1118@hotmail.com>
2024-12-19 13:30:26 +0800

    Update sildbar_20241112.py

--- sildbar_20241112.py
+++ sildbar_20241112.py
@@ -7,13 +7,12 @@ from data_transfer_page import data_transfer
 import seaborn as sns
 
 
-st.write("123456")
+st.write("66666")
 # Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#MainMenu {
-  visibility: hidden;
-}
-"""
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 def data_analysis():
